7c005aa715d2beff8775f31ec2ae59e2057e6979
tiagotaq <101513606+tiagotaq@users.noreply.github.com>
2025-10-18 10:30:04 -0300

    Adicionando armazenamento de artefatos, retries e repport

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    openMode: 0,
+    runMode: 2
+  },
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
